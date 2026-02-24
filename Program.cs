cb44b4ca17821856745f479d216a24b547ee5b38
enzoreal100 <enzoreal100@gmail.com>
2026-02-24 20:22:27 -0300

    feat: invalid number program

--- Program.cs
+++ Program.cs
@@ -26,7 +26,23 @@
 
 // programa que calcule a soma dos numeros do array
 
-int[] nums = {10, 20, 30, 40, 50};
-int soma = 0;
-foreach(int i in nums) soma += i;
-Console.WriteLine($"A soma dos números é {soma}");
+// int[] nums = {10, 20, 30, 40, 50};
+// int soma = 0;
+// foreach(int i in nums) soma += i;
+// Console.WriteLine($"A soma dos números é {soma}");
+
+// Criar um rograma que só aceita numeros positivos, continua solicitando novo valor
+
+int num;
+
+do
+{
+    Console.WriteLine("Digite um número positivo");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num < 0)
+    {
+        Console.WriteLine("Número inválido");
+    }
+} while (num < 0);
+Console.WriteLine($"Numero escolhido: {num}");
